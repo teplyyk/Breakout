b187bfae7ae964c2feb3f9e35c91eaa51bd7ac74
andrew-gresyk <andrew-gresyk@users.noreply.github.com>
2023-05-12 19:24:50 +0300

    Added proportionally sized window to the ball script

--- Assets/ScriptForRealBall.cs
+++ Assets/ScriptForRealBall.cs
@@ -2,28 +2,34 @@
 
 public class ScriptForRealBall : MonoBehaviour
 {
-    Vector3 Veloсity = GameSettings.BallStartingVelocity;
+	Vector3 Veloсity = GameSettings.BallStartingVelocity;
 
-    void FixedUpdate()
-    {
-        transform.Translate(Veloсity * Time.deltaTime);
-    }
-    void OnGUI()
-    {
-        if (GUILayout.Button("Press Me"))
-            Debug.Log("Hello!");
-    }
+	void FixedUpdate()
+	{
+		transform.Translate(Veloсity * Time.deltaTime);
+	}
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        var tag = collision.gameObject.tag;
-        if (tag == "floor")
-        {
-            // TODO: add game over
-        }
-        else
-        {
-            Veloсity.y = -Veloсity.y;
-        }
-    }
+	void OnGUI()
+	{
+		Rect rect = new Rect(
+			Screen.width / 4,
+			Screen.height / 4,
+			Screen.width / 2,
+			Screen.height / 2);
+
+		GUI.Box(rect, "Game Over");
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		var tag = collision.gameObject.tag;
+		if (tag == "floor")
+		{
+			// TODO: add game over
+		}
+		else
+		{
+			Veloсity.y = -Veloсity.y;
+		}
+	}
 }
